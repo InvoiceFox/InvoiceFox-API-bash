417e6bc858ed4b17f410d4083868aba53c79c8de
jimny <jimny@ubuntu.ubuntu-domain>
2011-10-15 08:19:34 -0400

    separated the bash client from API examples for less confusion

--- webgoo/TEST_refaktor-utils.r
+++
@@ -1,8 +0,0 @@
->> gen-where-filter [ name 1 ]
-== "name = 1"
->> gen-where-filter [ name "asdasd1" ]
-== "name = 'asdasd1'"
->> gen-where-filter [ name "asdasd'1" ]
-== "name = 'asdasd''1'"
->> gen-where-filter [ name "Janko's" age 213 ]
-== "name = 'Janko''s' AND age = 213"
